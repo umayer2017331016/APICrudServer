f1a34a95b9bb0f1dc2b51787ca7275a61203d05b
umayer2017331016 <44854824+umayer2017331016@users.noreply.github.com>
2023-10-08 10:24:58 +0600

    Nothing

---
+++ APICrud/Data/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APICrud.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
